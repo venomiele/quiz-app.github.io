421756de8b82bc3d6daef1bd23622e7d6f3a6525
venomiele <atrandafir35@yahoo.com>
2021-12-21 04:40:30 -0800

    added score and logic for the correct answers

--- index.js
+++ index.js
@@ -3,37 +3,47 @@ const quizData = [
     a: "Andrei is 18 years old.",
     b: "Andrei is 20 years old.",
     c: "Andrei is 22 years old.",
-    correct: "Andrei is 22 years old."
+    correct: "c"
     },
     { question: "What is the programming language Andrei likes the most ?",
     a: "Javascript",
     b: "Ruby",
     c: "C++",
-    correct: "Javascript"
+    correct: "a"
     },
     { question: "Will Andrei get a job as a developer in the next year?",
     a: "Yes",
     b: "Of course",
     c: "Sure",
-    correct: "Yes"
+    correct: "a"
     },
     { question: "Will You have a great day today?",
     a: "Yes",
     b: "Of course",
     c: "Sure",
-    correct: "Sure"
+    correct: "c"
+     },
+    { question: "What is Andrei's favorite animal?",
+a: "Dog",
+b: "Cat",
+c: "Wolf",
+correct: "b"
     }
 ];
 
 let container = document.querySelector(".hero-container")
 let question = document.querySelector(".question");
-let a_1 = document.getElementById("a");
-let b_2 = document.getElementById("b");
-let c_3 = document.getElementById("c");
+let a_1 = document.getElementById("answer1");
+let b_2 = document.getElementById("answer2");
+let c_3 = document.getElementById("answer3");
 let btn = document.querySelector(".btn");
 let chosen = document.querySelectorAll(".select");
 let error = document.querySelector(".error");
+let scoreBox = document.querySelector(".score-box");
+let scoreText = document.querySelector(".score");
 
+let score = 0;
+let decision = undefined;
 let currentQuestion = 0;
 btn.disabled = true;
 let checkArray = [];
@@ -72,9 +82,34 @@ b_2.innerHTML = currentData.b;
 c_3.innerHTML = currentData.c; 
 }
 
+function scoreNumber () {
+    chosen.forEach(function(inputs){
+        if(inputs.checked) {
+            decision = inputs.id;     
+        }
+    });
+    return decision;
+    }
+
+
 
 btn.addEventListener("click", () => {
+       
+        let clientDecision = scoreNumber();    
+        if(clientDecision === quizData[currentQuestion].correct) {
+            score++;
+        }       
         currentQuestion++;
-        blockBtn();
-         loadQuiz();
+         blockBtn();
+     
+
+        if(currentQuestion < quizData.length) {
+            loadQuiz();
+        } else {
+            container.style.display = "none";
+            scoreBox.style.display = "block";
+            scoreText.innerHTML = `You have answered correctly to ${score} out of 5 questions!`
+                     }
+         
+        
 });
